5666cbe6b5ad47e69fa0d95335af2bc828d190c3
Soren Malling <soren.malling@gmail.com>
2015-10-28 18:04:00 +0100

    Output count and items you've done today

---
+++ ihavedone.py
@@ -0,0 +1,21 @@
+import todoist
+from datetime import date
+import sys
+
+if len(sys.argv) != 2:
+    print 'You must give a token as parameter'
+    sys.exit(1)
+
+token = sys.argv[1]
+
+api = todoist.TodoistAPI(token)
+today = date.today().isoformat()
+
+completeditems = api.get_all_completed_items(since = today + 'T00:00')
+
+if len(completeditems["items"]) >= 1:
+    print len(completeditems["items"])
+    for d in completeditems["items"]:
+        print d["completed_date"] + ' --> ' + d["content"]
+else:
+    print 'API returned no items'
