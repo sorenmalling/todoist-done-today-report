e4186e2f3347d4af962afd902a0fd26e8e8f03fa
Soren Malling <soren.malling@gmail.com>
2015-10-29 08:06:38 +0100

    Better conditional check

--- ihavedone.py
+++ ihavedone.py
@@ -2,7 +2,7 @@ import todoist
 from datetime import date
 import sys
 
-if len(sys.argv) != 2:
+if len(sys.argv) >= 2:
     print 'You must give a token as parameter'
     sys.exit(1)
 
@@ -13,7 +13,7 @@ today = date.today().isoformat()
 
 completeditems = api.get_all_completed_items(since = today + 'T00:00')
 
-if len(completeditems["items"]) >= 1:
+if 'items' in completeditems and len(completeditems['items']) > 0:
     print len(completeditems["items"])
     for d in completeditems["items"]:
         print d["completed_date"] + ' --> ' + d["content"]
